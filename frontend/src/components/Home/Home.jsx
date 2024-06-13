c866b67e70ca1649a1c4d00d35e43d2eb33dc255
code-abhash <abhashraj751@gmail.com>
2024-06-13 14:21:06 +0530

    adding info button

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -35,6 +35,7 @@ import Navbar from "./Navbar";
 import Dashboard from "./Dashboard";
 import Realtime from "./Realtime";
 import Panel from "./Panel";
+import Infobutton from "../Infobutton/Infobutton";
 import "./Home.css";
 
 const Home = () => {
@@ -44,15 +45,24 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="p-4">
-        <p className="text-xl font-bold mb-2">Dashboard</p>
+        <p className="text-xl font-bold mb-2">Dashboard <Infobutton message={`This dashboard has info like:
+- New Patients: It tells about new patients enrolled 
+- Active Patients: It tells about the patients still active
+- Total Patients: Total aptients number
+- Visitors: New visitors numbers`}/></p>
         <Dashboard />
       </div>
       <div className="p-4">
-        <p className="text-xl font-bold mb-2">Disease Panel</p>
+        <p className="text-xl font-bold mb-2">Disease Panel <Infobutton message={`This is disease panel with drop down to select disease.
+          The health status button to go to disaese summary page`}/></p>
         <Panel />
       </div>
       <div className="p-4">
-        <p className="text-xl font-bold mb-2">Real Time Analysis</p>
+        <p className="text-xl font-bold mb-2">Real Time Analysis <Infobutton message={`This has the real time graphs showing the vitals vs days line graph.
+          -Heart beat
+          -blood pressure
+          -spo2
+          -Temperature`}/></p>
         <Realtime />
       </div>
     </div>
